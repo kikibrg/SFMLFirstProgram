e07a93c09920623541246c0092f1b8799216f320
Kilian <wishii64@outlook.fr>
2025-10-17 19:51:42 +0200

    Initial setup and check if working

---
+++ build/SFMLFirstProgram/CMakeFiles/SFMLFirstProgram.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Users/Kilian/Documents/Code_projects/SFMLFirstProgram/build/bin/SFMLFirstProgram.exe"
+  "C:/Users/Kilian/Documents/Code_projects/SFMLFirstProgram/build/bin/SFMLFirstProgram.exe.manifest"
+  "C:/Users/Kilian/Documents/Code_projects/SFMLFirstProgram/build/bin/SFMLFirstProgram.pdb"
+  "CMakeFiles/SFMLFirstProgram.dir/src/main.cpp.obj"
+  "CMakeFiles/SFMLFirstProgram.dir/src/main.cpp.obj.d"
+  "libSFMLFirstProgram.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFMLFirstProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
